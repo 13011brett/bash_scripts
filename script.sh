4e440fecb0122a080989ce46a9af2245e204ff97
13011brett <brett@scarletttech.com>
2023-03-02 19:51:12 -0500

    Allow for creation of DB.

--- script.sh
+++ script.sh
@@ -1,11 +1,50 @@
 #!/usr/bin/env bash
 
-echo "Hello World"
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then #Check if running as sudo level access.
+    echo "Not running as Sudo / Root. Please ensure your permissions are correct."
+    exit
+fi
 
-if [ ! -f /etc/init.d/mysql ]
+exitOnError(){
+    echo "$1" # Simply exit on error and echo the parameter.
+    exit
+}
+
+updateAndInstall(){
+    apt-get update -y && apt-get upgrade -y
+    apt-get install mysql-server -y
+}
+
+# createNewDBUser(){
+
+# }
+
+createNewDB(){
+    echo "Enter a DB name that you would like to create." #Could do an if to see if it's blank or not.
+    read DBNAME
+    mysql -e "CREATE DATABASE ${DBNAME} /*\!40100 DEFAULT CHARACTER SET utf8 */;" && echo "${DBNAME} was created successfully." || exitOnError "${DBNAME} could not be created."
+    read teee
+    
+    
+}
+
+
+
+
+
+
+if [ ! -f /etc/init.d/mysql ] # Check if MySql exists
 then
-    echo "File does not exist"
+    echo "File does not exist. Updating packages and installing SQL."
+    updateAndInstall
+
+    
+
 else    
     /etc/init.d/mysql status
+    echo "MySQL is installed"
+    createNewDB
+    # read -s rootpasswd
+    # echo "${rootpasswd}"
 
 fi
